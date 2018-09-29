ff361c3271b728373be15e885297d23f97840d93
55 <hi@im55.cc>
2018-09-30 07:54:24 +0800

    Auto translate slug

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Handlers\ImageUploadHandler;
 use App\Http\Requests\TopicRequest;
 use App\Models\Category;
 use App\Models\Topic;
 use Auth;
 use Illuminate\Http\Request;
-use App\Handlers\ImageUploadHandler;
 
 
 class TopicsController extends Controller
@@ -24,8 +24,12 @@ class TopicsController extends Controller
         return view('topics.index', compact('topics'));
     }
 
-    public function show(Topic $topic)
+    public function show(Request $request, Topic $topic)
     {
+        // URL 矫正
+        if (!empty($topic->slug) && $topic->slug != $request->slug) {
+            return redirect($topic->link(), 301);
+        }
         return view('topics.show', compact('topic'));
     }
 
@@ -40,7 +44,7 @@ class TopicsController extends Controller
         $topic->fill($request->all());
         $topic->user_id = Auth::id();
         $topic->save();
-        return redirect()->route('topics.show', $topic->id)->with('message', '创建成功！');
+        return redirect()->to($topic->link())->with('message', '创建成功！');
     }
 
     public function edit(Topic $topic)
@@ -55,7 +59,7 @@ class TopicsController extends Controller
         $this->authorize('update', $topic);
         $topic->update($request->all());
 
-        return redirect()->route('topics.show', $topic->id)->with('message', '更新成功！');
+        return redirect()->to($topic->link())->with('message', '更新成功！');
     }
 
     public function destroy(Topic $topic)

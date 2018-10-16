45192df2af8a2499694720296c86903f6972aa1a
55 <hi@im55.cc>
2018-10-17 07:13:43 +0800

    User can delete reply

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -4,6 +4,7 @@ namespace App\Observers;
 
 use App\Models\Reply;
 use App\Notifications\TopicReplied;
+
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
@@ -27,5 +28,10 @@ class ReplyObserver
         $topic->increment('reply_count', 1);
         // 通知作者话题被回复了
         $topic->user->notify(new TopicReplied($reply));
+    }
+
+    public function deleted(Reply $reply)
+    {
+        $reply->topic->decrement('reply_count', 1);
     }
 }

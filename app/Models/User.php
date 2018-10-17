8e28b0335dfb328861d0de9089bbcb2e2a01e00a
vagrant <vagrant@homestead>
2018-10-17 23:14:46 +0000

    Admin can manage users

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,8 +3,8 @@
 namespace App\Models;
 
 use Auth;
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
@@ -13,6 +13,7 @@ class User extends Authenticatable
     use Notifiable {
         notify as protected laravelNotify;
     }
+
     public function notify($instance)
     {
         // 如果要通知的人是当前用户，就不必通知了！
@@ -23,13 +24,36 @@ class User extends Authenticatable
         $this->laravelNotify($instance);
     }
 
+    public function setPasswordAttribute($value)
+    {
+        // 如果值的长度等于 60，即认为是已经做过加密的情况
+        if (strlen($value) != 60) {
+            // 不等于 60，做密码加密处理
+            $value = bcrypt($value);
+        }
+
+        $this->attributes['password'] = $value;
+    }
+
+    public function setAvatarAttribute($path)
+    {
+        // 如果不是 `http` 子串开头，那就是从后台上传的，需要补全 URL
+        if ( ! starts_with($path, 'http')) {
+
+            // 拼接完整的 URL
+            $path = config('app.url') . "/uploads/images/avatars/$path";
+        }
+
+        $this->attributes['avatar'] = $path;
+    }
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','introduction','avatar'
+        'name', 'email', 'password', 'introduction', 'avatar'
     ];
 
     /**
@@ -60,6 +84,6 @@ class User extends Authenticatable
     {
         $this->notification_count = 0;
         $this->save();
-            $this->unreadNotifications->markAsRead();
+        $this->unreadNotifications->markAsRead();
     }
 }

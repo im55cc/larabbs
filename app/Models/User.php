dd062f91e475dad488214bbc20b8c999a5f978c6
55 <hi@im55.cc>
2018-10-24 06:51:03 +0800

    Keep track of user last visited time

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,7 @@ class User extends Authenticatable
 {
     use Traits\ActiveUserHelper;
     use HasRoles;
+    use Traits\LastActivedAtHelper;
     use Notifiable {
         notify as protected laravelNotify;
     }
